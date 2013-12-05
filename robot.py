cf30e3878ad8d0a10af28941842d725a39924542
Ravi Kotecha <kotecha.ravi@gmail.com>
2013-12-05 21:01:53 +0000

    ememy locations

--- robot.py
+++ robot.py
@@ -1,8 +1,29 @@
 import rg
 
 class Robot:
+
+    def attack(self):
+        pass
+
+    def converge(self):
+        pass
+
+
+    def friendly_locations(self, locations):
+        return filter(locations, lambda x: x.get('robot_id'))
+
+    def enemy_locations(self, locations):
+        return filter(locations, lambda x: not x.get('robot_id'))
+
+    def get_moves(self, locations):
+        pass
+
+
     def act(self, game):
-        our_locations = [x["location"] for x in game['robots'].itervalues() if x.get('robot_id')]
+        all_locations = [x["location"] for x in game["robots"]]
+
+        our_locations = self.friendly_locations(all_locations)
+
         avg_robot_x = 0
         avg_robot_y = 0
         n_robots = 0
@@ -16,6 +37,6 @@ class Robot:
         delta_x = avg_robot_x - my_x
         delta_y = avg_robot_y - my_y
         if abs(delta_x) > abs(delta_y):
-            return ['move', (1 if delta_x > 0 else -1 ,0)]
+            return ['move', (my_x + (1 if delta_x > 0 else -1) , my_y)]
         else:
-            return ['move', (0, 1 if delta_y > 0 else -1)]
+            return ['move', (my_x, my_y+(1 if delta_y > 0 else -1))]
